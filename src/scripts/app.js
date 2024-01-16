d01c336ecdc3708fbd3c2a07976b56f7ca34b955
André Lima <dj_andre_lima@hotmail.com>
2024-01-16 05:19:11 -0300

    Criando tabela dos lanternas

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,36 +1,94 @@
-const player = document.querySelector(".player")
-let getPlayerPosition;
+const player = document.querySelector(".table")
+const player2 = document.querySelector(".table2")
 
 players = {
     andre: {
-        position: 0,
         pic: "./src/images/andre.webp",
         name: "André",
-        trophy: 0
+        trophy: 11,
+        losers: 4
     },
     marcio: {
-        position: 0,
         pic: "./src/images/marcio.webp",
         name: "Márcio",
-        trophy: 0
+        trophy: 7,
+        losers: 56
     },
     junior: {
-        position: 0,
         pic: "./src/images/junior.webp",
         name: "Júnior",
-        trophy: 0
+        trophy: 9,
+        losers: 4
     },
 }
 
 
-player.innerHTML = createPlayer(players.junior)
 
-function createPlayer(player) {
+function createPlayerChampion(player, position, id) {
 
     return `
-            <h2 id="player-pos">${player.position}</h2>
-            <img id="player-pic" src="${player.pic}" alt="player" >
-            <h2 id="player-name" >${player.name}</h2>
-            <h2 id="player-trophy" >${player.trophy}</h2>
+            <h2 class="player-pos" id= ${id}>${position}</h2>
+            <img class="player-pic" id= ${id} src="${player.pic}" alt="player" >
+            <h2 class="player-name" id= ${id} >${player.name}</h2>
+            <h2 class="player-trophy" id= ${id} >${player.trophy}</h2>
             `
 }
+
+function createPlayerLoser(player, position, id) {
+
+    return `
+            <h2 class="player-pos" id= ${id}>${position}</h2>
+            <img class="player-pic" id= ${id} src="${player.pic}" alt="player" >
+            <h2 class="player-name" id= ${id} >${player.name}</h2>
+            <h2 class="player-trophy" id= ${id} >${player.losers}</h2>
+            `
+}
+
+function createTable() {
+
+    const first = document.createElement("div")
+    const second = document.createElement("div")
+    const third = document.createElement("div")
+
+    const loserA = document.createElement("div")
+    const loserB = document.createElement("div")
+    const loserC = document.createElement("div")
+
+    first.className = second.className = third.className = "player"
+    loserA.className = loserB.className = loserC.className = "player"
+
+    let sortedPlayers = [ players.andre, players.junior, players.marcio ]
+    let losers = [ players.andre, players.junior, players.marcio ]
+
+    sortedPlayers.sort( (a, b) => {
+        if(b.trophy !== a.trophy) {
+            return b.trophy - a.trophy
+        }
+    })
+
+    losers.sort( (a, b) => {
+        if(b.losers !== a.losers) {
+            return b.losers - a.losers
+        }
+    })
+
+    first.innerHTML = createPlayerChampion(sortedPlayers[0], 1, "first")
+    second.innerHTML = createPlayerChampion(sortedPlayers[1], 2, "second")
+    third.innerHTML = createPlayerChampion(sortedPlayers[2], 3, "third")
+
+    player?.append(first)
+    player?.append(second)
+    player?.append(third)
+
+    loserA.innerHTML = createPlayerLoser(losers[0], 1, "first")
+    loserB.innerHTML = createPlayerLoser(losers[1], 2, "second")
+    loserC.innerHTML = createPlayerLoser(losers[2], 3, "third")
+
+    player2?.append(loserA)
+    player2?.append(loserB)
+    player2?.append(loserC)
+}
+
+
+createTable()
+
