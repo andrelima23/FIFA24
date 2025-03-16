6c64c7c3732ca8b941c21739c14e81133636b3c4
André Lima <69849061+andrelima23@users.noreply.github.com>
2025-03-16 05:10:13 -0300

    Atualizar o players.js

--- src/scripts/players.js
+++ src/scripts/players.js
@@ -2,8 +2,8 @@ export const players = {
     andre: {
         pic: "./src/images/andre.webp",
         name: "André",
-        trophies: 14,
-        losers: 4,
+        trophies: 16,
+        losers: 1,
         fifa2024: {
             trophies: 75,
             losers: 15
@@ -16,8 +16,8 @@ export const players = {
     marcio: {
         pic: "./src/images/marcio.webp",
         name: "Márcio",
-        trophies: 4,
-        losers: 13,
+        trophies: 5,
+        losers: 16,
         fifa2024: {
             trophies: 23,
             losers: 60
@@ -30,7 +30,7 @@ export const players = {
     junior: {
         pic: "./src/images/junior.webp",
         name: "Júnior",
-        trophies: 6,
+        trophies: 7,
         losers: 7,
         fifa2024: {
             trophies: 22,
@@ -62,24 +62,24 @@ export const scorers = {
     wood: {
         pic: "https://fifastatic.fifaindex.com/FIFA24/players/192123.png",
         name: "Wood",
-        goals: 5
+        goals: 6
     },
     mbappe: {
         pic: 'https://fifastatic.fifaindex.com/FIFA24/players/231747.png',
         name: "Mbappé",
-        goals: 5,
+        goals: 6,
         goals2024: 39
     }, 
     vlahovic: {
         pic: "https://cdn.sofifa.net/players/246/430/25_270.png",
         name: "Vlahović",
-        goals: 4,
+        goals: 5,
         goals2024: 23
     },
     salah: {
         pic: "https://cdn.sofifa.net/players/209/331/25_270.png",
         name: "Salah",
-        goals: 7,
+        goals: 8,
         goals2024: 13
     },
     haller: {
